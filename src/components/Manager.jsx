11ecc480ae00b90f5d9f4763bf9a6eee1092b11f
Sunnysines <ghosalsayantan293@gmail.com>
2026-01-22 11:19:49 +0530

    made some changes in the funtionality of the eye button.

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,43 +1,38 @@
 import React from 'react'
 import { useRef, useState, useEffect} from 'react'
 const Manager = () => {
+ 
     const Passwordref = useRef();
     const ref = useRef();
+    
     const [form, setForm] = useState({site: "", username: "", password: ""})
     const [passwordArray, setpasswordArray] = useState([])
 
+
+    const [showPasswordState, setShowPasswordState] = useState(false)
+
     useEffect(() => {
         let passwords = localStorage.getItem("passwords");
-        let passwordsArray;
         if (passwords) {
             setpasswordArray(JSON.parse(passwords));
         }
     }, [])
 
-    
 
     const showPassword = () => {
-        Passwordref.current.type = "text";
-        if(ref.current.src.includes("public/hidden.png")){
-            ref.current.src = "public/eye.png"
-            Passwordref.current.type = "password";
-        }
-        else{
-            ref.current.src = "public/hidden.png"
-            Passwordref.current.type = "text";
-        }
-        
-
+        setShowPasswordState(!showPasswordState)
     }
+
     const Savepassword = () => {
         setpasswordArray([...passwordArray, form]);
         localStorage.setItem("passwords", JSON.stringify([...passwordArray, form]));
         console.log(...passwordArray, form);
-
     }
+
     const handleChange = (e) => {
         setForm({...form, [e.target.name]: e.target.value})
     }   
+
     return (
         <>
             <div className="absolute inset-0 -z-10 h-full w-full bg-blue-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
@@ -55,9 +50,12 @@ const Manager = () => {
                         <input value={form.username} onChange={handleChange} placeholder='Enter Username' type="text" className="rounded-full bg-white px-4 py-1 border border-blue-500 w-full" name="username" id="" />
                         <div className='relative'>
 
-                        <input ref={Passwordref} value={form.password} onChange={handleChange} placeholder='Enter Password' type="password" className="rounded-full bg-white px-4 py-1 border border-blue-500 w-full" name="password" id="" />
+                        
+                        <input ref={Passwordref} value={form.password} onChange={handleChange} placeholder='Enter Password' type={showPasswordState ? "text" : "password"} className="rounded-full bg-white px-4 py-1 border border-blue-500 w-full" name="password" id="" />
+                        
                         <span className='absolute right-[3px] top-[4px] cursor-pointer' onClick={showPassword} >
-                            <img ref={ref} className='p-1' width={26} src="\public\eye.png" alt="eye" />
+                            
+                            <img ref={ref} className='p-1' width={26} src={showPasswordState ? "/public/hidden.png" : "/public/eye.png"} alt="eye" />
                         </span>
                         </div>
                     </div>
@@ -83,21 +81,17 @@ const Manager = () => {
                         </thead>
                         <tbody className='bg-blue-100'>
                             {passwordArray.map((item, index) => {
-                                return <tr>
+                                return <tr key={index}>
                                 <td className='py-2 border border-white text-center min-w-32'><a href={item.site} target=''>{item.site}</a></td>
                                 <td className='py-2 border border-white text-center min-w-32'>{item.username}</td>
                                 <td className='py-2 border border-white text-center min-w-32'>{item.password}</td>
                             </tr>
                             })}
-                        
-                        
-                          
                         </tbody>
                     </table>}
                 </div>
             </div>
         </>
-
     )
 }
 
